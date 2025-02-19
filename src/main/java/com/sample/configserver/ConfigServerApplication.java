0e59603fbc3cd7889f371e53a3cce09c16bbc85c
PRADEEP KUMAR PALAI <pradeepkumarpalai@PRADEEPs-MacBook-Air.local>
2025-02-19 15:10:09 +0530

    changes for configutation

--- src/main/java/com/sample/configserver/ConfigServerApplication.java
+++ src/main/java/com/sample/configserver/ConfigServerApplication.java
@@ -2,12 +2,22 @@ package com.sample.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableConfigServer
+@RestController
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
+	@GetMapping("/hlleo")
+	public String gethello() {
+
+		return "hello";
+	}
 }
